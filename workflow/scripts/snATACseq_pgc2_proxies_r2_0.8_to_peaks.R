e1d311b78c36aad431f23de200e71a507399d2ee
Dazcam <camerond@cardiff.ac.uk>
2021-10-19 14:32:02 +0100

    Corrections and batching for BiomaRt - INCOMPLETE

--- workflow/scripts/snATACseq_pgc2_proxies_r2_0.8_to_peaks.R
+++ workflow/scripts/snATACseq_pgc2_proxies_r2_0.8_to_peaks.R
@@ -12,6 +12,9 @@
 
 ## Info  ------------------------------------------------------------------------------
 
+# STATUS: Incomplete: BiomaRt choking one large batches even in split batch data 
+# corrupted may need to download dsSNP VCF file and cross ref manually
+
 # Extract proxy SNPs of PGC3 index SNPs with r2 > 0.8
 # 287 SNPs in PGC3_SZ_index_SNPs.xlsx
 # 9 SNPs failed 
@@ -35,9 +38,8 @@ REGIONS <- c("FC", "GE")
 CELL_TYPES <- c("FC-ExN", "FC-InN", "FC-RG", "FC-MG", "FC-N.undef", "GE-InN", "GE-RG")
 TOKEN <- ""
 
-
-# Read in PGC3 index SNPs one is not an rsID just removed it
-snps <- read_excel("Desktop/single_nuclei/snRNAseq/tests/PGC3_SZ_index_SNPs.xlsx") %>%
+##  Read in PGC3 index SNPs one is not an rsID just removed it  ------------------------
+snps_0.1 <- read_excel("Desktop/single_nuclei/snRNAseq/tests/PGC3_SZ_index_SNPs_0.1.xlsx") %>%
   dplyr::select(`top-index`) %>%
   filter(!grepl('8:4180090_T_A', `top-index`)) %>%
   base::as.data.frame(snps)
@@ -46,12 +48,12 @@ snps <- read_excel("Desktop/single_nuclei/snRNAseq/tests/PGC3_SZ_index_SNPs.xlsx
 # With default of 0.01. LD taken from 1000Gs phase 3 - CEU population
 snps_in_LD <- LDlinkR::LDproxy_batch(snp = snps, token = TOKEN)
 
-# Remove SNPs that failed proxy batch call from snp list
+## Remove SNPs that failed proxy batch call from snp list -----------------------------
 error_snps <- c('rs2494638', 'rs12514660', 'rs2914025', 'rs55858161',
                 'rs62152282', 'rs35026989', 'rs11241041', 'rs650520')
 snps_no_errors <- setdiff(snps, error_snps)
 
-# Get proxy SNPs with R2 > 0.8 for all index SNPs
+##  Get proxy SNPs with R2 > 0.8 for all index SNPs. ----------------------------------
 for (SNP in snps$`top-index`) {
   
   skip_to_next <- FALSE
@@ -60,8 +62,8 @@ for (SNP in snps$`top-index`) {
   tryCatch(
     
     
-    proxies <<- read_delim(paste0("~/Desktop/single_nuclei/snRNAseq/tests/PGC3_proxy_SNPs/", SNP, ".txt"), 
-                                 delim = "\t", escape_double = FALSE, 
+    proxies <<- read_delim(paste0("~/Desktop/single_nuclei/snRNAseq/tests/PGC3_proxy_SNPs/", 
+                                  SNP, ".txt"), delim = "\t", escape_double = FALSE, 
                                  trim_ws = TRUE),
     
     error = function(e) {
@@ -76,10 +78,12 @@ for (SNP in snps$`top-index`) {
   
   # Generate stats
   index_in_proxies <- SNP %in% proxies$Coord
+  index_in_proxies_r2_0.8 <- SNP %in% proxies_r2_0.8$Coord
   snps_in_proxies <- nrow(proxies)
-  snps_in_proxies_r2_0.8 <- nrow(proxies)
+  snps_in_proxies_r2_0.8 <- nrow(proxies_r2_0.8)
   cat(paste0('\n', nrow(proxies), ' snps in ', SNP, ' proxies table ... \n'))
   cat(paste0('\nChecking if ', SNP, ' index SNP is in proxies table: ', index_in_proxies, '\n'))
+  cat(paste0('\nChecking if ', SNP, ' index SNP is in proxies r2 > 0.8 table: ', index_in_proxies_r2_0.8, '\n'))
   
   if (exists("snp_proxies_df")) {
     
@@ -99,22 +103,61 @@ for (SNP in snps$`top-index`) {
 
 # Retain only unique SNPs in list - reduces from 237K to 1.7K SNPs!
 cat('\nRetain only unique SNPs  ... \n')
-snp_proxies_unique_df <- snp_proxies_df %>% distinct(R2, .keep_all = TRUE)
+snp_proxies_unique_df <- snp_proxies_df %>% distinct(Coord, .keep_all = TRUE)
 scz_snps <- as.vector(snp_proxies_unique_df$Coord)
 scz_snps
+
+# Remove weird IDs: "ALU_umary_ALU_20" etc.
+scz_snps <- scz_snps[grepl('rs', scz_snps)]
+
 cat(paste0(length(scz_snps), ' SNPs retained.\n'))
+
+library(httr)    
+set_config(use_proxy(url="10.3.100.207",port=8080))
+SNPs <- ncbi_snp_query(scz_snps)
 
 ## Get hg38 base postions for rsIDs using biomaRt -------------------------------------
 cat('\nUsing BiomaRt to get hg38 base postions for SNP rsIDs  ... \n')
 ensembl <- useEnsembl("ENSEMBL_MART_SNP", dataset = "hsapiens_snp")
-SNPs <- getBM(attributes=c("refsnp_id",
+# Need to batch the query: https://support.bioconductor.org/p/23684/
+SNPs_a <- getBM(attributes=c("refsnp_id",
                            "chr_name",
                            "chrom_start",
                            "chrom_end"),
               filters ="snp_filter", 
-              values = scz_snps, 
+              values = scz_snps[1:50000], 
               mart = ensembl, 
               uniqueRows=TRUE)
+Sys.sleep(1)
+SNPs_b <- getBM(attributes=c("refsnp_id",
+                             "chr_name",
+                             "chrom_start",
+                             "chrom_end"),
+                filters ="snp_filter", 
+                values = scz_snps[50001:100000], 
+                mart = ensembl, 
+                uniqueRows=TRUE)
+Sys.sleep(1)
+SNPs_c <- getBM(attributes=c("refsnp_id",
+                             "chr_name",
+                             "chrom_start",
+                             "chrom_end"),
+                filters ="snp_filter", 
+                values = scz_snps[100001:150000], 
+                mart = ensembl, 
+                uniqueRows=TRUE)
+Sys.sleep(1)
+SNPs_d <- getBM(attributes=c("refsnp_id",
+                             "chr_name",
+                             "chrom_start",
+                             "chrom_end"),
+                filters ="snp_filter", 
+                values = scz_snps[150001:length(scz_snps)], 
+                mart = ensembl, 
+                uniqueRows=TRUE)
+
+SNPs <- rbind(SNPs_a, SNPs_b, SNPs_c, SNPs_d)
+
 cat(paste0(nrow(SNPs), ' SNPs retained.\n'))
 
 # Some SNPs include duplicates due to chr patches - I removed these
@@ -174,3 +217,4 @@ cat('Done.\n')
   
 #--------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------
+
