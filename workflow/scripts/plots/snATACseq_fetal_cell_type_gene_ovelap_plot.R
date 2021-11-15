8b282a809b64b8da115b39d9f295a119f6da2b8b
Dazcam <camerond@cardiff.ac.uk>
2021-11-15 12:57:56 +0000

    Added fetal vs. ExN-2 plot

--- workflow/scripts/plots/snATACseq_fetal_cell_type_gene_ovelap_plot.R
+++ workflow/scripts/plots/snATACseq_fetal_cell_type_gene_ovelap_plot.R
@@ -26,8 +26,8 @@ skene_matrix <- readRDS(paste0(DATA_DIR, 'skene_overlap_matrix.rds'))
 magma_matrix <- read_table(paste0(DATA_DIR, 'magma_fetal.vs.adult_summary.tsv'))
 
 ##  Plot ------------------------------------------------------------------------------
+# Figure 4 - Adult vs. fetal grid
 fig_4 <- skene_matrix %>%
-  add_column(percent = prcnt) %>%
   mutate(percent = sprintf("%0.3f", percent)) %>%
   ggplot(aes(x=Var1, y=Var2, fill = 'white')) + 
   geom_tile(color = "black", size = 0.5, fill = '#DBF3FA') +
@@ -45,15 +45,17 @@ fig_4 <- skene_matrix %>%
   xlab("") + 
   ylab("") +
   coord_equal(ratio = 1) 
-  
+
+
+# Figure ext data fig 1 - Adult vs. fetal grid
+magma_matrix_filt <- magma_matrix %>%
+  filter(!grepl("_no_FC", VARIABLE))
 
 # Fix y-axis labels
-
-y_axis_labels <- rev(gsub("_", "-", as.vector(unique(magma_matrix$VARIABLE))))
+y_axis_labels <- rev(gsub("_", "-", as.vector(unique(magma_matrix_filt$VARIABLE))))
 y_axis_labels <- gsub("-no-", " no ", y_axis_labels)
 
-ext_data_fig_1 <- magma_matrix %>%
-  filter(!grepl("_no_FC", VARIABLE)) %>%
+ext_data_fig_1 <- magma_matrix_filt %>%
   ggplot(aes(x = -log10(as.numeric(P)), y = factor(VARIABLE, rev(levels(factor(VARIABLE)))))) +
   geom_bar(stat = "identity", fill = c('#CEE5FD', '#CEE5FD', '#CEE5FD', '#CEE5FD', '#CEE5FD',
                                        '#CEE5FD', '#CEE5FD', '#CEE5FD', '#CEE5FD', '#CEE5FD',
@@ -81,6 +83,35 @@ ext_data_fig_1 <- magma_matrix %>%
   xlab(expression(-log[10](P))) +
   ylab('Cell type')
 
+# Figure 4B - fetal vs. fetal grid
+magma_matrix_no_skene <- magma_matrix %>%
+  filter(!grepl("skene", VARIABLE))
+
+# Fix y-axis labels
+y_axis_labels <- rev(gsub("_", "-", as.vector(unique(magma_matrix_no_skene$VARIABLE))))
+y_axis_labels <- gsub("-no-", " no ", y_axis_labels)
+
+ext_data_fig_2 <- magma_matrix_no_skene %>%
+  ggplot(aes(x = -log10(as.numeric(P)), y = factor(VARIABLE, rev(levels(factor(VARIABLE)))))) +
+  geom_bar(stat = "identity", fill = c('#CEE5FD', '#CEE5FD', '#CEE5FD', '#3CBB75FF', '#3CBB75FF', 
+                                       '#3CBB75FF', '#CEE5FD', '#CEE5FD', '#CEE5FD', '#CEE5FD'), 
+           color = 'black') +
+  geom_vline(xintercept=-log10(0.05/54), linetype = "dashed", color = "black") +
+  geom_vline(xintercept=-log10(0.05), linetype = "dotted", color = "black") +
+  theme_bw() +
+  #  ggtitle(toupper(TITLE)) +
+  theme(plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black", size = 1),
+        plot.title = element_text(hjust = 0.5),
+        axis.title.x = element_text(colour = "#000000", size = 14),
+        axis.title.y = element_text(colour = "#000000", size = 14),
+        axis.text.x  = element_text(colour = "#000000", size = 12, vjust = 0.5),
+        axis.text.y  = element_text(colour = "#000000", size = 12)) +
+  scale_y_discrete(labels = y_axis_labels) +
+  xlab(expression(-log[10](P))) +
+  ylab('Cell type')
 
 
 # Save plots
@@ -90,10 +121,16 @@ tiff(paste0(PLOT_DIR, "Fig_4.tiff"), height = 30, width = 30, units='cm',
 fig_4
 dev.off()
 
-# Fig 4B - Fetal cell type gene overlap plot
+# Fig extended data figure 1
 tiff(paste0(PLOT_DIR, "ext_data_fig_1.tiff"), height = 30, width = 30, units='cm', 
      compression = "lzw", res = 300)
 ext_data_fig_1
+dev.off()
+
+# Fig extended data figure 2
+tiff(paste0(PLOT_DIR, "ext_data_fig_2.tiff"), height = 30, width = 30, units='cm', 
+     compression = "lzw", res = 300)
+ext_data_fig_2
 dev.off()
 
 # -------------------------------------------------------------------------------------
