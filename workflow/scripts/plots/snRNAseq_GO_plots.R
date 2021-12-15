c9cac8feb680c9e50a779f97c56293adfd8e097c
Dazcam <camerond@cardiff.ac.uk>
2021-12-15 13:28:23 +0000

    Input changed - NB curated

--- workflow/scripts/plots/snRNAseq_GO_plots.R
+++ workflow/scripts/plots/snRNAseq_GO_plots.R
@@ -13,12 +13,15 @@ cat('\nLoading packages ... \n\n')
 library(tidyverse)
 library(cowplot)
 library(janitor)
+library(readxl)
 
 ##  Info  -----------------------------------------------------------------------------
 
-# 1. Extract fold-change enrichments and FDR for everything FDR < 0.05 
-# 2. Y = cell-types, X = Processes
+# 1. Table has 2 sets of col values - https://stackoverflow.com/questions/62613535
+# 2. Y = cell-types, X = GO terms
 # 3. Circle size = fold-change enrichment, colour FDR / P-value 
+# Changing Fold enrichment dot radius:
+# https://ggplot2-book.org/scale-other.html / https://stackoverflow.com/questions/56098080 
 
 ##  Initialise variables  -------------------------------------------------------------
 DATA_DIR <- "Dropbox/BRAY_sc_analysis/snRNA-seq/GO/"
@@ -27,69 +30,60 @@ CELL_TYPES <- c("FC_ExN_2", "FC_ExN_3", "FC_ExN_4", "FC_ExN_5", "FC_InN_1",
                 "GE_InN_1", "GE_InN_2", "Hipp_ExN_4", "Hipp_ExN_6", "Thal_ExN_5", 
                 "Thal_ExN_9")
 
-##  Load data -------------------------------------------------------------------------
-for (CELL_TYPE in CELL_TYPES) {
-  
-  cat("\nImporting ", CELL_TYPE, " data\n")
-  GO <- read_delim(paste0(DATA_DIR, CELL_TYPE, " top decile GO.txt"), 
-                   delim = "\t", escape_double = FALSE, 
-                   trim_ws = TRUE, progress = FALSE) %>%
-    filter(FDR < 0.05) %>%
-    top_n(-10)  # select the terms with the lowest FDR
-    
-  cat(paste0("\nNumber of terms with FDR < 0.05: ", nrow(GO), "\n")) 
-    
-  GO <- GO %>%  mutate(cell_type = rep(CELL_TYPE, nrow(GO))) %>%
-    select(Term, FDR, `Fold Enrichment`, cell_type) 
-    
-  GO_plot <- ggplot(data = GO, aes(y = Term, x = cell_type, 
-                          color = FDR, size = `Fold Enrichment`)) + 
-    geom_point() +
-    scale_color_gradient(low = "red", high = "blue") +
-    theme_bw() + 
-    ylab("") + 
-    xlab("") + 
-    ggtitle("GO enrichment analysis")
-  
-  assign(paste0(CELL_TYPE, '_GO'), GO)
-  assign
-  
-  # Save plot
-  tiff(paste0(PLOT_DIR, CELL_TYPE, "_GO.tiff"), height = 15, width = 30, units='cm', 
-       compression = "lzw", res = 300)
-  print(GO_plot)
-  dev.off()
-  
-}
 
-terms_to_remove <- paste('trans-synaptic signaling', 'anterograde', 'trans-synaptic signaling', 
-                           'cell-cell adhesion', 'axonogenesis', 'regulation of transmembrane transport', 
-                           'ion transmembrane transport', 'cell development', 'eye development', 
-                           'system process', sep = '|')
+##  Load data  ------------------------------------------------------------------------
+GO_DATA <- read_excel(paste0(DATA_DIR, "GOterms_to_plot_for_figure.xlsx"), skip = 2, col_names = F) %>%  
+  mutate(across(everything(), ~replace_na(.x, 0))) # Get rid of NAs
 
-# Plot all together
-GO_ALL <- rbind(FC_ExN_2_GO, FC_ExN_3_GO, FC_ExN_4_GO, FC_ExN_5_GO, FC_InN_1_GO, 
-      GE_InN_1_GO, GE_InN_2_GO, Hipp_ExN_4_GO, Hipp_ExN_6_GO, Thal_ExN_5_GO, 
-      Thal_ExN_9_GO) %>%
-  filter(!str_detect(Term, terms_to_remove))
+colnames(GO_DATA) <- c("Term", "FC_ExN_2-FE", "FC_ExN_2-FDR", "FC_ExN_3-FE", "FC_ExN_3-FDR", 
+  "FC_ExN_4-FE", "FC_ExN_4-FDR", "FC_ExN_5-FE", "FC_ExN_5-FDR", "FC_InN_1-FE", 
+  "FC_InN_1-FDR", "GE_InN_1-FE", "GE_InN_1-FDR", "GE_InN_2-FE", "GE_InN_2-FDR", 
+  "Hipp_ExN_4-FE", "Hipp_ExN_4-FDR", "Hipp_ExN_6-FE", "Hipp_ExN_6-FDR", "Thal_ExN_5-FE", 
+  "Thal_ExN_5-FDR", "Thal_ExN_9-FE", "Thal_ExN_9-FDR")
 
-GO_ALL_plot <- ggplot(data = GO_ALL, aes(y = Term, x = cell_type, 
-                                 color = -log10(FDR), size = `Fold Enrichment`)) + 
+# Create factor for y-axis order
+ORDERED_LIST <- GO_DATA %>% select(Term) %>%  
+#  mutate(Term = gsub(".*~", "", Term)) %>%  # Remove GO numbers
+#  mutate(Term = R.utils::capitalize(Term)) %>%
+  pull() %>%
+  as_factor()
+  
+# Prep data
+GO_DATA <- GO_DATA %>% 
+  pivot_longer(-Term) %>% 
+  separate(name, into = c("cell_type", "Score"), '-') %>% 
+  pivot_wider(names_from = Score, values_from = value) %>%
+  mutate_at('cell_type', str_replace_all, "_", "-") %>%
+  # mutate(bio_term = gsub(".*~", "", Term)) %>%
+  # mutate(bio_term = R.utils::capitalize(bio_term)) %>%
+  rename(`Fold Enrichment` = FE) 
+
+# Plot
+GO_plot <- ggplot(data = GO_DATA, aes(y = factor(Term, level = rev(ORDERED_LIST)), x = cell_type, 
+                                         color = -log10(FDR), size = `Fold Enrichment`)) +
   geom_point() +
+  theme_bw() +
+  theme(plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
+        #      panel.grid.major = element_blank(), 
+        #    panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black", size = 1),
+        plot.title = element_text(hjust = 0.5),
+        axis.title.x = element_text(colour = "#000000", size = 14),
+        axis.title.y = element_text(colour = "#000000", size = 14),
+        axis.text.x  = element_text(colour = "#000000", size = 12, vjust = 0.5),
+        axis.text.y  = element_text(colour = "#000000", size = 12),
+        legend.text=element_text(size = 10)) +
   scale_color_gradient(low = "blue", high = "red") +
-  theme_bw() + 
   theme(axis.text.x = element_text(colour = "#000000", angle = 45, vjust = 1, hjust = 1)) +
   ylab("") + 
-  xlab("") + 
-  ggtitle("GO enrichment analysis") 
+  xlab("") +
+  scale_radius(limits = c(1, 5), range = c(1,6))
 
 # Save plot
-tiff(paste0(PLOT_DIR, "ALL_GO.tiff"), height = 15, width = 30, units='cm', 
+tiff(paste0(PLOT_DIR, "ALL_GO.tiff"), height = 30, width = 40, units='cm', 
      compression = "lzw", res = 300)
-print(GO_ALL_plot)
+print(GO_plot)
 dev.off()
-
-
 
 # -------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------
